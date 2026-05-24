5018e9d56d99597a73fd9ceca82a2b35651fa9c1
OscarXie <oscarxiejianmin@qq.com>
2026-05-24 19:20:54 +0800

    新增 GSConvE、SNI 和 GSConvE2 模块，并修复 GSConv 中的卷积参数传递错误，新增Slim Neck V2混合改进网络

--- models/common.py
+++ models/common.py
@@ -103,8 +103,9 @@ class GSConv(nn.Module):
     def __init__(self, c1, c2, k=1, s=1, g=1, act=True):
         super().__init__()
         c_ = c2 // 2
-        self.cv1 = Conv(c1, c_, k, s, None, g, act)
-        self.cv2 = Conv(c_, c_, 5, 1, None, c_, act)
+        # fixing the bug of missing `d`dilation when params passing to Conv, which should be 1 for no dilation in GSConv
+        self.cv1 = Conv(c1, c_, k, s, None, g, 1, act)
+        self.cv2 = Conv(c_, c_, 5, 1, None, c_, 1, act)
 
     def forward(self, x):
         x1 = self.cv1(x)
@@ -150,6 +151,77 @@ class GSBottleneck(nn.Module):
 
     def forward(self, x):
         return self.conv_lighting(x) + self.shortcut(x)
+
+
+class SNI(nn.Module):
+    """
+    https://github.com/AlanLi1997/rethinking-fpn
+    soft nearest neighbor interpolation for up-sampling
+    secondary features aligned
+    """
+
+    def __init__(self, c1=0, c2=0, up_f=2):
+        super(SNI, self).__init__()
+        self.us = nn.Upsample(None, up_f, "nearest")
+        self.alpha = 1 / (up_f**2)
+
+    def forward(self, x):
+        return self.alpha * self.us(x)
+
+
+class GSConvE(nn.Module):
+    """
+    GSConv enhancement for representation learning: generate various receptive-fields and
+    texture-features only in one Conv module
+    https://github.com/AlanLi1997/slim-neck-by-gsconv
+    """
+
+    def __init__(self, c1, c2, k=1, s=1, g=1, d=1, act=True):
+        super().__init__()
+        c_ = c2 // 2
+        self.cv1 = Conv(c1, c_, k, s, None, g, d, act)
+        self.cv2 = nn.Sequential(
+            nn.Conv2d(c_, c_, 3, 1, 1, bias=False),
+            nn.Conv2d(c_, c_, 3, 1, 1, groups=c_, bias=False),
+            nn.GELU(),
+        )
+
+    def forward(self, x):
+        x1 = self.cv1(x)
+        x2 = self.cv2(x1)
+        y = torch.cat((x1, x2), dim=1)
+        # shuffle
+        y = y.reshape(y.shape[0], 2, y.shape[1] // 2, y.shape[2], y.shape[3])
+        y = y.permute(0, 2, 1, 3, 4)
+        return y.reshape(y.shape[0], -1, y.shape[3], y.shape[4])
+
+
+class GSConvE2(nn.Module):
+    # enhancement lightweight conv
+
+    def __init__(self, c1, c2, k=1, s=1, g=1, d=1, act=True):
+        super().__init__()
+        c_ = c2 // 4
+        self.cv1 = Conv(c1, c_, k, s, k // 2, g, d, act)
+        self.cv2 = Conv(c_, c_, 9, 1, k // 2, c_, d, act)
+        self.cv3 = Conv(c_, c_, 13, 1, k // 2, c_, d, act)
+        self.cv4 = Conv(c_, c_, 17, 1, k // 2, c_, d, act)
+
+    def forward(self, x):
+        y = torch.cat(
+            (
+                self.cv1(x),
+                self.cv2(self.cv1(x)),
+                self.cv3(self.cv1(x)),
+                self.cv4(self.cv1(x)),
+            ),
+            dim=1,
+        )
+        # shuffle
+        y = y.reshape(y.shape[0], 2, y.shape[1] // 2, y.shape[2], y.shape[3])
+        output = y.permute(0, 2, 1, 3, 4)
+
+        return output.reshape(output.shape[0], -1, output.shape[3], output.shape[4])
 
 
 class DWConv(Conv):
